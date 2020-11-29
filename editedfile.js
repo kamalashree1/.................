2b4271ad8b4752ed9e5a4e72ed02134dac7b8d68
kamalashree1 <51331580+kamalashree1@users.noreply.github.com>
2020-11-29 21:46:32 +0530

    Create editedfile.js

---
+++ editedfile.js
@@ -0,0 +1,29 @@
+class Paper{
+  
+    constructor(x,y,width,height){
+        var option = { 
+         
+          'restitution':0.3,
+            'friction':0.5,
+            'density':1.2,
+            'isStatic' : false
+         
+       }
+        this.body = Bodies.rectangle(x,y,width,height,option); 
+        this.width = width;
+        this.height = height;
+        //Matter.Body.setAngle(this.body,angle)
+        World.add(world, this.body);
+        } 
+        display(){
+             var pos = this.body.position
+             //var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        //rotate(angle);
+         rectMode(CENTER); 
+         fill("brown"); 
+         rect(0,0,this.width,this.height);
+        pop();
+            }
+}
